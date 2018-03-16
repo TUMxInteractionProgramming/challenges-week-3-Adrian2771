8497444a3f05439172c3004add8da397402e403a
Valentin Adrian <adrian_iclozan27@yahoo.ro>
2018-03-16 11:39:32 +0200

    switchChannel added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 /*challenge 6*/
 console.log('app is alive');
+
+function switchChannel(channelName){
+	console.log('Tuning into channel ' + channelName);
+	var test = channelName;
+	document.getElementById('actual-channel').textContent = test;
+}
+
+var channelCollection = document.querySelectorAll('#channels ul li');
+
+console.log(channelCollection);
+
+for(let i = 0; i < channelCollection.length; i++){
+	channelCollection[i].addEventListener('click', function(){
+		var tmp = this.textContent;
+		tmp = tmp.replace(/\s+>?/g, '');
+		//console.log(tmp);
+		switchChannel(tmp);
+	}); 
+}
+
+
+//switchChannel();
